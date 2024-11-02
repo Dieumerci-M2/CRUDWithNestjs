aee6ad8e925ec981080dec85bf98e649361e627c
Dieumerci MUHINDO <dieumerci.md09@gmail.com>
2024-11-02 12:51:19 +0200

    feat : configure DB on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory : (configService : ConfigService)=>({
+        type: 'postgres',
+        host: configService.get("DB_HOST"),
+        port: +configService.get("DB_PORT"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB-NAME"),
+        entities: [join(process.cwd(), "dist/**/*.entity.js")], 
+        // Do not set synchronize to true on real project
+        synchronize: true,
+      })
+     
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
