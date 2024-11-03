9b659a485aae83ab260f313def68d50f4f47492f
Dieumerci MUHINDO <dieumerci.md09@gmail.com>
2024-11-03 19:47:53 +0200

    feat : install postgres DB and config it

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { CitiesModule } from './cities/cities.module';
         port: +configService.get("DB_PORT"),
         username: configService.get("DB_USERNAME"),
         password: configService.get("DB_PASSWORD"),
-        database: configService.get("DB-NAME"),
+        database: configService.get("DB_NAME"),
         entities: [join(process.cwd(), "dist/**/*.entity.js")], 
         // Do not set synchronize to true on real project
         synchronize: true,
